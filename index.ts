de769dddc8345873077fa385abe68cddb8ad5ddd
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-26 13:17:33 +0100

    using typescript to detect errors

--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
 import axios from "axios";
 const url = "https://jsonplaceholder.typicode.com/todos/1"
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(res => {
-  console.log(res.data);
+  const todo = res.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    the Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it completed: ${completed}
+  `);
 })
