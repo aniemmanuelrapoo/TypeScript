bf11731da9b2d436265c8dbdb36019a605e448c7
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-27 04:47:54 +0100

    fixing any type error

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -33,5 +33,5 @@ const logNumber: (i: number) => void = (i: number) => {
 //when to use annotation
 //1. function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
-const coordinates = JSON.parse(json);
+const coordinates: {x: number; y: number} = JSON.parse(json);
 console.log(coordinates);
